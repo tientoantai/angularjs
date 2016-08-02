ff789560444ede5d9fba6e7c6061aea60330ebf1
Tien Toan Tai <nguyenminhtien61vn@gmail.com>
2016-08-02 14:21:28 +0700

    display statistic

--- ToDoList/todo.js
+++ ToDoList/todo.js
@@ -1,17 +1,37 @@
 var app = angular.module('todoList', []);
 app.controller('todoListController', function($scope) {
-	$scope.list = [{taskName: "buy Macbook", done: false}, {taskName: "buy Motobike", done: false}];
+	$scope.list = [
+		{taskName: "buy Macbook", done: false}, 
+		{taskName: "buy Motobike", done: false}
+	];
 	$scope.addTodo = function(){
 		$scope.list.push({taskName: $scope.newTask, done: false});
 		$scope.newTask = '';
 	};
 
-	$scope.checkDone = function(){
+	$scope.checkDone = function(todo){
 		//todo is in view 
-		if (this.todo.done){
-			alert (this.todo.taskName + " is done!")
+		if (todo.done){
+			alert ("task \"" + todo.taskName + "\" is done!")
 		}else{
-			alert (this.todo.taskName + " is NOT done!")
+			alert ("task \"" + todo.taskName + "\" is NOT done!")
 		}
-	}
+	};
+
+	$scope.totalTask = function(){
+		return $scope.list.length;
+	};
+
+	$scope.totalDoneTask = function(){
+		var countDoneTask = 0;
+		angular.forEach($scope.list, function(task){
+			countDoneTask += (task.done) ? 1 : 0 ;
+		});
+		return countDoneTask;
+	};
+
+	$scope.donePercent = function(){
+		if ($scope.totalTask() == 0) return 0;
+		return window.Math.round($scope.totalDoneTask() * 100 / $scope.totalTask());
+	};
 });
